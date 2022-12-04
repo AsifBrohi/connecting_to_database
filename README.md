c8d8bdc9793b27684beeaf2f21faa68accaf44b7
Asif Brohi <asifbrohi@hotmail.co.uk>
2022-12-04 00:35:15 +0000

    new updates

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # connecting_to_database
+
+## How to connect to python database 
+
+The general way to connect to a database using python is seen below 
+
+example is using mysql
+
+create variable = mysql.connector.connect(
+    host = "localhost",
+    user = "Username",
+    password = "password",
+    database = "datbase name"
+)
--- mysql_connect.py
+++ mysql_connect.py
@@ -25,26 +25,26 @@ except:
 # except:
 #     print("could not create database")
 
-cursor = example_db.cursor()
+# cursor = example_db.cursor()
 
-product_menu_tb = '''CREATE TABLE IF NOT EXISTS products (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
-    product_name TEXT NOT NULL, 
-    price FLOAT NOT NULL
-);'''
+# product_menu_tb = '''CREATE TABLE IF NOT EXISTS products (
+#     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+#     product_name TEXT NOT NULL, 
+#     price FLOAT NOT NULL
+# );'''
 
-get_products = '''SELECT * from products;''' # select query 
+# get_products = '''SELECT * from products;''' # select query 
 
-try:
-    cursor.execute(product_menu_tb)
-    cursor.execute(get_products)
-    output = cursor.fetchall() # fetching all the data in the table 
-    for view in output:
-        print(view) # showing data in terminal 
-    print("successful in creating a table")
-except:
-    print("unsuccesful")
+# try:
+#     cursor.execute(product_menu_tb)
+#     cursor.execute(get_products)
+#     output = cursor.fetchall() # fetching all the data in the table 
+#     for view in output:
+#         print(view) # showing data in terminal 
+#     print("successful in creating a table")
+# except:
+#     print("unsuccesful")
 
-finally:
-    cursor.close()
-    example_db.close()
+# finally:
+#     cursor.close()
+#     example_db.close()
